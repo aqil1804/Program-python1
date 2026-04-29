2b9511070c76046f6c8e58a633b25e88a56e9d75
aqil1804 <muh.aqil.afsal.pribadi7@gmail.com>
2026-04-29 14:59:28 +0800

    Create Program.py

---
+++ Program.py
@@ -0,0 +1,9 @@
+# program.py
+print("Program Penjumlahan Sederhana")
+
+a = int(input("Masukkan angka pertama: "))
+b = int(input("Masukkan angka kedua: "))
+
+hasil = a + b
+
+print("Hasil penjumlahan =", hasil)
