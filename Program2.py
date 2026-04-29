b40d9ea67cdcdb3e149f99188402dd5819cf8270
aqil1804 <muh.aqil.afsal.pribadi7@gmail.com>
2026-04-29 15:01:40 +0800

    Create Program2.py

---
+++ Program2.py
@@ -0,0 +1,9 @@
+
+print("Program Cek Bilangan Genap / Ganjil")
+
+angka = int(input("Masukkan sebuah angka: "))
+
+if angka % 2 == 0:
+    print("Angka", angka, "adalah bilangan GENAP")
+else:
+    print("Angka", angka, "adalah bilangan GANJIL")
